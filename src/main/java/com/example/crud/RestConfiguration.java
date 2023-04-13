0a2d8c11317ae3943c6068dbef59556adf0d8200
desaop03 <desaop03@santaana.local>
2023-04-13 08:02:40 -0600

    configurando jakarta

--- src/main/java/com/example/crud/RestConfiguration.java
+++ src/main/java/com/example/crud/RestConfiguration.java
@@ -1,9 +1,18 @@
 package com.example.crud;
 
+import com.example.crud.resources.PostResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
-@ApplicationPath("api")
+@ApplicationPath("/api")
 public class RestConfiguration extends Application {
-    
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(PostResource.class);
+        return classes;
+    }
 }
--- src/main/java/com/example/crud/resources/PostResource.java
+++ src/main/java/com/example/crud/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.example.crud.resources;
 
 import com.example.crud.repositories.PostsRepository;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
@@ -8,14 +9,18 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("post")
+@RequestScoped
 public class PostResource {
 
     @Inject
     PostsRepository postsRepository;
 
     @GET
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response getPosts() {
         return postsRepository.obtenerPosts();
     }
