a80fb6b1ea9f9a73b5a3f13b856a82f602693122
roonmorton <roonmorton@gmail.com>
2023-04-13 21:10:46 -0600

    configurando health

---
+++ src/main/java/com/example/crud/health/LivenessHealthCheck.java
@@ -0,0 +1,31 @@
+package com.example.crud.health;
+
+import java.time.LocalDateTime;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@RequestScoped
+public class LivenessHealthCheck implements HealthCheck {
+
+    private String time;
+
+    @PostConstruct
+    private void init() {
+        this.time = LocalDateTime.now().toString();
+    }
+
+    @Override
+    public HealthCheckResponse call() {
+
+        return HealthCheckResponse.builder().name("Liveness Check")
+                .withData("status", "ALIVE")
+                .withData("from", time)
+                .build();
+
+    }
+
+}
